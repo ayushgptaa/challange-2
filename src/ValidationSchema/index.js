6c4c7bcc4fd72ae0592cc0078e42424ec44c59b0
ayushgptaa <ayush.gupta.jk@gmail.com>
2022-11-26 21:26:55 +0530

    fix Yup validation

--- src/ValidationSchema/index.js
+++ src/ValidationSchema/index.js
@@ -9,7 +9,9 @@ const validationSchema = Yup.object({
     })
     .required('Aadhar number is required'),
   panNumber: Yup.string()
-    .test('len', 'Pan number should be 10 digits', (val) => val.length === 10)
+    .test('len', 'Pan number should be 10 digits', (val) => {
+      if (val) return val.length === 10
+    })
     .required('Pan number is required'),
   employeeType: Yup.string().required('Employee type is required'),
   joiningDate: Yup.date().required('Joining date is required'),
